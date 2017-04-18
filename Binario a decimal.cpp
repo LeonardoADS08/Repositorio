daf2734e47ff1e15bad80b64365bd31b97a54706
Leonardo Arteaga dos Santos <LeonardoADS08@gmail.com>
2017-04-17 23:05:52 -0400

    Binario a decimal

--- Binario a decimal.cpp
+++ Binario a decimal.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int x,aux=0,i=0,j=0,dec=0;
+    int x, aux = 0, i = 0, j = 0, dec = 0;
     int bin[100];
-    cin>>x;
+    cin >> x;
     while (x) {
         bin[i] = x%10;
         x /= 10;
         i++;
     }
     i--;
-    while (i>=j) {
-        aux = aux*2+bin[i];
+    while (i >= j) {
+        aux = aux * 2 + bin[i];
         i--;
     }
-    cout<<aux<<endl;
+    cout << aux << endl;
     return 0;
 }
