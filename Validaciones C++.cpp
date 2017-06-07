ac0221c8284d697f22a56d140d9536c6d32c59b7
Leonardo Arteaga dos Santos <LeonardoADS08@gmail.com>
2017-06-07 13:46:10 -0400

    Update Validaciones C++.cpp

--- Validaciones C++.cpp
+++ Validaciones C++.cpp
@@ -1,3 +1,19 @@
+bool verif_solo_numeros(char cad[])
+{
+	bool k = true;
+	int i = 0;
+	while(i < strlen(cad) && k==true)
+	{
+		if((cad[i] >= '0') && (cad[i] <= '9')) 
+			i++;
+		else 
+			k = false;
+	}
+	if (strlen(cad)==0) 
+		k =  false;
+	return k;
+}
+
 int leerNumeros()
 {
 	char numero[200];
